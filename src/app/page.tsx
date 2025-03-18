261f4d3503a1f16fa603faacf97ca04db1ca7ce0
John Carr <johnny.carr1@grainger.com>
2025-03-17 21:20:22 -0500

    Updating workflow and changing text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <h1 >
+          Keep on keepin on 
+        </h1>
         <div>
           Welcome to my world, won&apos;t you come on in?
         </div>
